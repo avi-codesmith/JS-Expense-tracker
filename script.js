91b18bf637eda2e956b0904937b12c4f3d8019b6
avi-codesmith <avi.codesmith@gmail.com>
2025-03-16 10:15:07 +0530

    Implement in Input

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ const category = document.getElementById("category-select");
 const addBtn = document.getElementById("add-btn");
 const expensesTableBody = document.getElementById("expense-table-body");
 const totalCol = document.getElementById("total-amount");
+const sound = new Audio("errorSound.mp3");
 let totalAmount = 0;
 
 inputs.forEach((input, index) => {
@@ -24,16 +25,62 @@ const addTd = () => {
   const amountValue = Number(amount.value);
   const dateValue = date.value;
 
-  if (!amountValue) {
-    alert("Please Enter Amount!");
+  if (!selectedCategory && !dateValue && !amountValue) {
+    inputs.forEach((input) => {
+      sound.play();
+      input.classList.add("move");
+      setTimeout(() => {
+        input.classList.remove("move");
+      }, 200);
+    });
     return;
-  }
-  if (!dateValue) {
-    alert("Please Enter or Select Date!");
+  } else if (!amountValue && !selectedCategory) {
+    sound.play();
+    amount.classList.add("move");
+    category.classList.add("move");
+    setTimeout(() => {
+      amount.classList.remove("move");
+      category.classList.remove("move");
+    }, 200);
     return;
-  }
-  if (!selectedCategory) {
-    alert("Please Select Category!");
+  } else if (!selectedCategory && !dateValue) {
+    sound.play();
+    category.classList.add("move");
+    date.classList.add("move");
+    setTimeout(() => {
+      category.classList.remove("move");
+      date.classList.remove("move");
+    }, 200);
+    return;
+  } else if (!amountValue && !dateValue) {
+    sound.play();
+    date.classList.add("move");
+    amount.classList.add("move");
+    setTimeout(() => {
+      date.classList.remove("move");
+      amount.classList.remove("move");
+    }, 200);
+    return;
+  } else if (!amountValue) {
+    sound.play();
+    amount.classList.add("move");
+    setTimeout(() => {
+      amount.classList.remove("move");
+    }, 200);
+    return;
+  } else if (!dateValue) {
+    sound.play();
+    date.classList.add("move");
+    setTimeout(() => {
+      date.classList.remove("move");
+    }, 200);
+    return;
+  } else if (!selectedCategory) {
+    sound.play();
+    category.classList.add("move");
+    setTimeout(() => {
+      category.classList.remove("move");
+    }, 200);
     return;
   }
 
